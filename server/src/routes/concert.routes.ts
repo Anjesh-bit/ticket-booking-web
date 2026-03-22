5366f7057e3586aa81c9921dd56dd785f2b603f9
Anjesh-bit <anjesh67890@gmail.com>
2026-03-22 20:48:45 +0545

    chore(apps): removed getAvailability redundant endpoints

--- server/src/routes/concert.routes.ts
+++ server/src/routes/concert.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { getAvailability, getConcertById, getConcerts } from "#controllers/concert.controller.js";
+import { getConcertById, getConcerts } from "#controllers/concert.controller.js";
 import { cache } from "#middlewares/cache/redis.middleware.js";
 import { asyncHandler } from "#middlewares/error/errorHandler.middleware.js";
 
@@ -12,12 +12,6 @@ router.get(
   "/:id",
   cache({ ttl: 300, keyGenerator: (req) => `concert:${req.params.id}` }),
   asyncHandler(getConcertById),
-);
-
-router.get(
-  "/:id/availability",
-  cache({ ttl: 10, keyGenerator: (req) => `availability:${req.params.id}` }),
-  asyncHandler(getAvailability),
 );
 
 export default router;
