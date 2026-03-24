44b57e64cccab002e5f3b8d230f00fb111dbe50c
Anjesh-bit <75300382+Anjesh-bit@users.noreply.github.com>
2026-03-24 22:03:24 +0545

    chore(app-core): package configuration files and typecheck in ci (#27)

--- server/src/cache/redis/client.ts
+++ server/src/cache/redis/client.ts
@@ -1,10 +1,8 @@
-import type { RedisClientType } from "redis";
-
 import { redisConnection } from "#connections/redis.js";
 import logger from "#lib/helpers/winston.helpers.js";
 
 class RedisClient {
-  private client: RedisClientType | null;
+  private client: ReturnType<typeof redisConnection.getClient> | null;
   private readonly defaultTTL: number;
 
   constructor() {
@@ -12,7 +10,7 @@ class RedisClient {
     this.defaultTTL = 3600;
   }
 
-  private getClient(): RedisClientType {
+  private getClient() {
     if (!this.client) throw new Error("Redis client not initialized");
     return this.client;
   }
---
+++ server/src/routes/index.ts
@@ -0,0 +1,4 @@
+import bookingRouter from "./booking.routes.js";
+import concertRouter from "./concert.routes.js";
+
+export { concertRouter, bookingRouter };
